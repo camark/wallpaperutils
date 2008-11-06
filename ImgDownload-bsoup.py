ceaff10e177fe76b33d740decde599e3572f4115
gm8pleasure@126.com <gm8pleasure@126.com@8f612990-e155-0410-aaed-c319d8fbff03>
2008-11-06 08:46:54 +0000

    Use map function

--- ImgDownload-bsoup.py
+++ ImgDownload-bsoup.py
@@ -25,18 +25,25 @@ url='http://www.trmusic.com.cn/blog/user1/198/archives/2006/897.asp'
 url_obj = urllib2.urlopen(url)
 
 start_url = 'http://image2.sina.com.cn/dongman/'
+
+def DownImgFromUrl(url):
+    img_src = url['src']
+    image_name = img_src.split('/')[-1]
+    urllib.urlretrieve(img_src,getDownloadDir()+'/'+image_name)
+    
 if url_obj.code == 200:
     url_data = url_obj.read()
     
     bs=BeautifulSoup(''.join(url_data))
     #print bs.prettify()
-    for img in bs.findAll('img'):
-        print img['src']
-        image_path = img['src']
-        
-        if image_path.startswith(start_url):
-            image_name = image_path.split('/')[-1]
-            urllib.urlretrieve(image_path,getDownloadDir()+'/'+image_name)
-            
+#    for img in bs.findAll('img'):
+#        print img['src']
+#        image_path = img['src']
+#        
+#        if image_path.startswith(start_url):
+#            image_name = image_path.split('/')[-1]
+#            urllib.urlretrieve(image_path,getDownloadDir()+'/'+image_name)
     
+    map(DownImgFromUrl,[m for m in bs.findAll('img') if m['src'].startswith(start_url)])
+                
 print 1
